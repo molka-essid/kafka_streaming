75fe398ac5ed113c7ff383fb93c9bdd1d9e3ba8c
molka <molka.essid@dauphine.tn>
2025-12-16 14:15:55 +0100

    1st version : working on the clean data

--- csv_lab/consumer_csv.py
+++ csv_lab/consumer_csv.py
@@ -1,1 +1,16 @@
-# Write your csv consumer code here
+from kafka import KafkaConsumer
+
+TOPIC = "csv-transactions"
+
+consumer = KafkaConsumer(
+    TOPIC,
+    bootstrap_servers="localhost:9092",
+    auto_offset_reset="earliest",
+    group_id="csv-consumer-group",
+    value_deserializer=lambda v: v.decode("utf-8")
+)
+
+print("Starting CLEAN CSV Consumer...")
+
+for msg in consumer:
+    print(f"Consumed: {msg.value}")
--- csv_lab/producer_csv.py
+++ csv_lab/producer_csv.py
@@ -1,1 +1,25 @@
-#Write your csv producer code here
+from kafka import KafkaProducer
+import time
+
+TOPIC = "csv-transactions"
+
+producer = KafkaProducer(
+    bootstrap_servers='127.0.0.1:9092'
+,  
+    value_serializer=lambda v: str(v).encode('utf-8')
+)
+
+print("Starting CLEAN CSV Producer...")
+
+with open("data/transactions.csv", "r") as f:
+    header = f.readline()  # skip header
+    for line in f:
+        line = line.strip()
+        print(f"Producing: {line}")
+        producer.send(TOPIC, value=line)
+        time.sleep(1)  # simulate streaming
+
+producer.flush()
+producer.close()
+
+print("Producer finished.")
